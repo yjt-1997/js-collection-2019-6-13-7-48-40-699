5e6b21e04fc502b9988cd5c431024c24b66f2db0
YOUNG YANG (ITA-ISDC-ISD-OOCLL/ZHA) <YANGYO7@OOCL.COM>
2019-07-10 13:57:48 +0800

    first commit

--- practices/filter/choose_even.js
+++ practices/filter/choose_even.js
@@ -1,8 +1,6 @@
 'use strict';
 
 function choose_even(collection) {
-
-  //implement here
+  return collection.filter(item => item % 2 == 0);
 }
-
 module.exports = choose_even;
--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -1,8 +1,12 @@
 'use strict';
 
 function grouping_count(collection) {
-
-  //implement here
+  var json = {};
+  collection.filter(function (item) {
+    if (json[item] == undefined)
+      json[item] = 1;
+    else json[item]++;
+  });
+  return json;
 }
-
 module.exports = grouping_count;
